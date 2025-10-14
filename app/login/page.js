9163260669dc0fe34a45ad9dc6f4bb31cd909146
Rhino Hospitality Group <rhinohospitalitygroup@unknown6a7483668398.attlocal.net>
2025-10-14 18:20:21 -0400

    Update login and update-password page.js to use ssr: false with dynamic imports

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const LoginPageClient = dynamic(() => import("./LoginPageClient"), { ssr: false });
--- app/update-password/page.js
+++ app/update-password/page.js
@@ -1,9 +1,8 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
-const UpdatePasswordPageClient = dynamic(
-  () => import("./UpdatePasswordPageClient"),
-  { ssr: false } // client-only
-);
+const UpdatePasswordPageClient = dynamic(() => import("./UpdatePasswordPageClient"), { ssr: false });
 
 export default function Page() {
   return <UpdatePasswordPageClient />;
